8f5d4a45c07d057921c566db28da95cbd00b08c2
Thomas MARTIN <thomas-martin35@hotmail.fr>
2026-01-07 15:03:24 +0100

    Initial Spring Boot project structure

---
+++ src/main/java/com/bmartin/immo/BmartinImmoBackApplication.java
@@ -0,0 +1,13 @@
+package com.bmartin.immo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmartinImmoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmartinImmoBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bmartin/immo/BmartinImmoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmartin.immo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmartinImmoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
